8ee139239ac56578a8971361c94515c233f758e3
Marcus Jang <makerpopo@gmail.com>
2015-08-15 05:13:30 +0900

    Added siteName to globals.js

--- config/globals.js
+++ config/globals.js
@@ -1,6 +1,7 @@
 module.exports = function(req) {
     return {
         title: '건전성 테스트 불합격 팀블로그',
+        siteName: 'snim.us',
         user: req.user,
         isMobile: req.useragent.isMobile
     }
