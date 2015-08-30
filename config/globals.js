b5d1dba7e2889c29371ad84c2419a64d4fd8f2a8
Marcus Jang <makerpopo@gmail.com>
2015-08-30 22:05:25 +0900

    Added email address on globals.js

--- config/globals.js
+++ config/globals.js
@@ -2,6 +2,7 @@ module.exports = function(req) {
     return {
         title: '건전성 테스트 불합격 팀블로그',
         siteName: 'snim.us',
+        email: 'contact [at] snim.us',
         user: req.user,
         isMobile: req.useragent.isMobile
     }
